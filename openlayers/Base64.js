bbefd36f063b8f50431ab0b368efed2e331df761
fredj <frederic.junod@camptocamp.com>
2012-02-13 17:29:57 +0100

    simplify base64 convertion

--- openlayers/Base64.js
+++ openlayers/Base64.js
@@ -15,16 +15,8 @@ function getTileDataURL(tile, callback, scope) {
             xhr.responseType = 'arraybuffer';
             xhr.onload = function(e) {
                 if (this.status === 200) {
-                    var bytes = new Uint8Array(this.response);
-                    var binary = '', i, len = bytes.byteLength;
-                    for (i = 0; i < bytes.byteLength; i+=8) {
-                        binary += String.fromCharCode(bytes[i    ], bytes[i + 1], bytes[i + 2], bytes[i + 3],
-                                                      bytes[i + 4], bytes[i + 5], bytes[i + 6], bytes[i + 7]);
-                    }
-                    for (i -= 4; i < bytes.byteLength; i++) {
-                        binary += String.fromCharCode(bytes[i]);
-                    }
-                    var base64 = btoa(binary);
+                    var bytes = Array.prototype.slice.call(new Uint8Array(this.response));
+                    var base64 = btoa(String.fromCharCode.apply(null, bytes));
                     var mime = 'image/' + (base64.substr(0, 11) == 'iVBORw0KGgo' ? 'png' : 'jpeg');
                     // jpeg = base64.substr(0, 3) == '/9g'
                     callback.call(scope, 'data:' + mime + ';base64,' + base64);
